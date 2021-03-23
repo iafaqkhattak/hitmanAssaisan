6346117cc600c0edc5f394eb97d8c6807d5f1293
Muhammad Afaq <iafaqkhattak@gmail.com>
2021-03-23 18:02:18 +0500

    Quick update

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public int speed = 3;
+    public int js = 3;
+
+    private Rigidbody2D rigidbody1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody1 = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float move = Input.GetAxis("Horizontal");
+        Debug.Log("move");
+        rigidbody1.velocity = new Vector2(move * speed, rigidbody1.velocity.y);
+
+
+
+
+    }
+}
---
+++ Assets/runningAndJump.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningAndJumpScript : MonoBehaviour
+{
+
+    public float speed =3;
+    public float js =3;
+
+
+        private Rigidbody2D rigidbody;
+    public Animator animate;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        Debug.Log("move");
+        rigidbody.velocity = new Vector2(move * speed, rigidbody.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            rigidbody.velocity = new Vector2(rigidbody.velocity.y, speed);
+
+        }
+    }
+}
